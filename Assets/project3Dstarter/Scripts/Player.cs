dcc7bac9a17b42c738171aeccf52cb313e59cca4
dr-matt-smith <dr_matt_smith@me.com>
2018-06-01 12:01:49 +0100

    added files to project

---
+++ Assets/project3Dstarter/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public static int score = 0;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Player.score++;
+		Destroy(other.gameObject);
+	}
+
+	private void Update()
+	{
+		print("Score: " + Player.score);
+	}
+}
